19ae108f3926df0931655ca87744b456fc8191bd
Horleryheancarh <oladipoolayinka7@gmail.com>
2024-03-23 14:06:36 +0100

    modified app

--- app/main.py
+++ app/main.py
@@ -4,12 +4,16 @@ from blog import models
 from blog.database import engine
 from blog.routers import user, blog, authentication
 
+import uvicorn
+
 
 app = FastAPI()
-
 
 models.Base.metadata.create_all(engine)
 
 app.include_router(authentication.router)
 app.include_router(user.router)
 app.include_router(blog.router)
+
+if __name__ == "__main__":
+	uvicorn.run(app, host='127.0.0.1', port=9000)
